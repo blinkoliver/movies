eae5f4d6280d34a13d51490388e792a7f171fefb
Anton Barouka <52159126+blinkoliver@users.noreply.github.com>
2019-11-21 00:01:35 +0300

    padaet release_date.slice

--- src/components/App.js
+++ src/components/App.js
@@ -37,49 +37,52 @@ class App extends React.Component{
     };
 
   render(){
-
       return (
           <Router>
-                  <header>
-                      <div className={'Container'}>
+              <header>
+                  <div className={'Container'}>
                       <div className={'home'}>
-                  <button><Link to={"/"}>Movie-Search</Link></button>
-                  <Route exact path={"/"}/>
-                </div>
+                        <button><Link to={"/"}>Movie-Search</Link></button>
+                        <Route exact path={"/"}/>
+                      </div>
 
-                <div className={'search'}>
-                    <Input onChange={event=>this.handleInput(event)}
-                           onBlur={this.onBlur}
-                           onFocus={this.onFocus}
-                    />
-                    {this.state.inputIsFocused&&
-                        <div id={'suggestSearch'}>
-                            {this.state.results.map(result =>
-                                <div key={result.id} id={'suggest'}>
-                                    <Link key={result.id} to={`/InformationPage/${result.id}`}>
-                                        <img src={`https://image.tmdb.org/t/p/w500${result.poster_path}`}
-                                             alt={result.title}/>
-                                        <div className={'suggestionLabel'}>
-                                            <span key={result.id}>{result.title}</span>
-                                            <span>({result.release_date.slice(0, 4)})</span>
-                                        </div>
-                                    </Link>
-                                </div>
-                            )}
-                        </div>
-                    }
-                </div>
+                      <div className={'search'}>
+                        <Input onChange={event=>this.handleInput(event)}
+                               onBlur={this.onBlur}
+                               onFocus={this.onFocus}
+                        />
+                        {this.state.inputIsFocused&&
+                            <div id={'suggestSearch'}>
+                                {this.state.results.map(result =>
+                                    <div key={result.id} id={'suggest'}>
+                                        <Link key={result.id} to={`/InformationPage/${result.id}`}>
+                                            <img src={`https://image.tmdb.org/t/p/w500${result.poster_path}`}
+                                                 alt={result.title}/>
+                                            <div className={'suggestionLabel'}>
+                                                <span key={result.id}>{result.title}</span>
+                                                <span>({result.release_date.slice(0, -6)})</span>
+                                            </div>
+                                        </Link>
+                                    </div>
+                                )}
+                            </div>
+                        }
+                      </div>
 
-                <div className={'personal'}>
-                    <button><Link to={"/Cabinet"}>Sign In</Link></button>
-                </div>
-              </div>
+                      <div className={'personal'}>
+                        <button><Link to={"/Cabinet"}>Sign In</Link></button>
+                      </div>
+                  </div>
               </header>
+
               <div className={'Content'}>
                   <Route path={'/InformationPage/:id'} component={InformationPage}/>
               </div>
-              <footer></footer>
-        </Router>
+
+              <footer>
+
+              </footer>
+          </Router>
       )
   };
 }
--- src/components/InformationPage.js
+++ src/components/InformationPage.js
@@ -1,31 +1,46 @@
 import React from "react";
 
 class InformationPage extends React.Component{
+ constructor(props) {
+     super(props);
+     this.state = {
+            post: []
+     };
+ }
 
-    state={
-      post:[]
-    };
+    fetchMovieInfo(){
+        fetch(`https://api.themoviedb.org/3/movie/${this.props.match.params.id}?api_key=6ed6e56030be8bc7d1821d5b302e302e&language=en-US`)
+            .then(information=>information.json())
+            .then(post=>{
+                    this.setState({post:post});
+                }
+            )
+    }
 
     componentDidMount() {
-    fetch(`https://api.themoviedb.org/3/movie/${this.props.match.params.id}?api_key=6ed6e56030be8bc7d1821d5b302e302e&language=en-US`)
-        .then(information=>information.json())
-        .then(post=>{
-            this.setState({post:post});
-            console.log(this.state.post)
-            }
-    )}
+        this.fetchMovieInfo()
+    }
 
     componentDidUpdate(prevProps) {
         if(this.props.match.params.id!==prevProps.match.params.id){
-            this.componentDidMount(this.props.match.params.id)
+            this.fetchMovieInfo(this.props.match.params.id)
         }
     }
 
     render(){
         return(
-            <div className={'App'}>
-                <div className={'App-header'}>
-                    <p>Title:{this.state.post.original_title}</p>
+            <div className={'Container'}>
+                <div className={'TitleBlock'}>
+                    <div className={'TitleBar'}>
+                        <h1>{this.state.post.original_title}</h1>
+                        <span>{this.state.post.release_date.slice(0,-6)}</span>
+
+                    </div>
+                    <div className={'RatingsWrapper'}>
+
+                    </div>
+                </div>
+                <div className={'SlateWrapper'}>
                     <img
                         src={`https://image.tmdb.org/t/p/w200${this.state.post.poster_path}`}
                         alt={this.state.post.original_title}
