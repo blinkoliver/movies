292e54533f3cdd9c7eb7a0c31c5e77e9de6701ba
Anton Barouka <52159126+blinkoliver@users.noreply.github.com>
2020-02-27 17:22:12 +0300

    add hook

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,55 +1,50 @@
 import React from "react";
 import Loading from "../components/Loading";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { connect } from "react-redux";
-import { getTrendingMovies } from "../actions/movies";
 
-class Home extends React.Component {
-  componentDidMount() {
-    this.props.getTrendingMovies(
+const Home = (props) => {
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  const [trendingMoviesLoading, setTrendingMoviesLoading] = useState(false);
+
+  const fetchTrendingMovies = async () => {
+    setTrendingMoviesLoading(true);
+    const res = await fetch(
       `https://api.themoviedb.org/3/trending/movie/week?api_key=6ed6e56030be8bc7d1821d5b302e302e`
     );
-  }
+    const trendingMovies = await res.json();
+    setTrendingMovies(trendingMovies.results);
+    setTrendingMoviesLoading(false);
+  };
 
-  render() {
-    if (this.props.trendingMoviesLoading) {
-      return <Loading/>;
-    }
-    return (
-      <div className={"Container"}>
-        <div>
-          <h1 style={{ color: "darkslategray" }}>Trending movies this week</h1>
-        </div>
-        <div className={"KnownForItems"} style={{ flexDirection: "column" }}>
-          {this.props.trendingMovies.slice(0, 6).map((result) => (
-            <div key={result.id} style={{ flexDirection: "row" }}>
-              <Link key={result.id} to={`/InformationPage/${result.id}`}>
-                <img
-                  src={`https://image.tmdb.org/t/p/w200${result.poster_path}`}
-                  alt={result.id}
-                />
-                {result.original_title}
-              </Link>
-              <h2 style={{ color: "darkslategray" }}>{result.overview}</h2>
-            </div>
-          ))}
-        </div>
+  useEffect(() => {
+    fetchTrendingMovies();
+  }, []);
+
+  console.log(props, trendingMovies, trendingMoviesLoading);
+
+  return trendingMoviesLoading ? (
+    <Loading />
+  ) : (
+    <div className={"Container"}>
+      <div>
+        <h1 style={{ color: "darkslategray" }}>Trending movies this week</h1>
       </div>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    trendingMovies: state.movies.trendingMovies,
-    trendingMoviesLoading: state.movies.trendingMoviesLoading
-  };
+      <div className={"KnownForItems"} style={{ flexDirection: "column" }}>
+        {trendingMovies.slice(0, 6).map((result) => (
+          <div key={result.id} style={{ flexDirection: "row" }}>
+            <Link key={result.id} to={`/InformationPage/${result.id}`}>
+              <img
+                src={`https://image.tmdb.org/t/p/w200${result.poster_path}`}
+                alt={result.id}
+              />
+              {result.original_title}
+            </Link>
+            <h2 style={{ color: "darkslategray" }}>{result.overview}</h2>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    getTrendingMovies: (url) => dispatch(getTrendingMovies(url))
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default Home;
